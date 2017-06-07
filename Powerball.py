c4fb976dd2bcb28e02fef452e8fb27f529de3655
Chris Sheehan <stcts02@moravian.edu>
2017-06-06 23:44:26 -0400

    User input works completely as specified after refactoring getNumbersFromUser() in Powerball.py

--- Powerball.py
+++ Powerball.py
@@ -1,4 +1,5 @@
 from __future__ import print_function
+from collections import Counter
 import sys
 
 nameAndFavNums = []
@@ -14,25 +15,34 @@ def getEmployeeNameFromUser():
     name = name + " " + sys.stdin.readline()
     nameAndFavNums.append(name.replace("\n", ""))
 
+
 def getNumbersFromUser():
-    print('Select 1st # (1 thru 69): ', end='')
-    if checkIfValidNum(sys.stdin.readline().replace("\n", "")):
-        nameAndFavNums.append(sys.stdin.readline().replace("\n", ""))
-    print('Select 2nd # (1 thru 69 excluding' + str(nameAndFavNums[1:]) + '): ', end='')
-    if checkIfValidNum(sys.stdin.readline().replace("\n", "")):
-        nameAndFavNums.append(sys.stdin.readline().replace("\n", ""))
-    print('Select 3rd # (1 thru 69 excluding' + str(nameAndFavNums[1:]) + '): ', end='')
-    if checkIfValidNum(sys.stdin.readline().replace("\n", "")):
-        nameAndFavNums.append(sys.stdin.readline().replace("\n", ""))
-    print('Select 4th # (1 thru 69 excluding' + str(nameAndFavNums[1:]) + '): ', end='')
-    if checkIfValidNum(sys.stdin.readline().replace("\n", "")):
-        nameAndFavNums.append(sys.stdin.readline().replace("\n", ""))
-    print('Select 5th # (1 thru 69 excluding' + str(nameAndFavNums[1:]) + '): ', end='')
-    if checkIfValidNum(sys.stdin.readline().replace("\n", "")):
-        nameAndFavNums.append(sys.stdin.readline().replace("\n", ""))
-    print('Select Power Ball # (1 thru 26): ', end='')
-    if checkIfValidNum(sys.stdin.readline().replace("\n", "")):
-        nameAndFavNums.append(sys.stdin.readline().replace("\n", ""))
+    pos = ['1st', '2nd', '3rd', '4th', '5th', 'Power Ball']
+    exclusion = '69'
+    userInputComplete = False
+    i = 0
+
+    while userInputComplete is False:
+        print('Select ' + pos[i] + ' # (1 thru ' + exclusion  + '): ', end='')
+        try:
+            line = int(raw_input())
+            if checkIfValidNum(line) is True:
+                nameAndFavNums.append(line)
+                i += 1
+                if i == 5:
+                    exclusion = '26'
+                else:
+                    if i >= 2:
+                        exclusion = '69 excluding ' + str(nameAndFavNums[1:-1]).strip("[").strip("]") + ', and ' + str(nameAndFavNums[-1])
+                    else:
+                        exclusion = '69 excluding ' + str(nameAndFavNums[1])
+                if i == 6:
+                    userInputComplete = True
+            else:
+                print("Invalid integer, try again.")
+        except ValueError:
+            print("Not an integer, try again.")
+
 
 def checkIfValidNum(num):
     if len(nameAndFavNums) == 0:
@@ -53,13 +63,20 @@ def checkIfValidNum(num):
 def writeToFile(line):
     employeePowerball = open("employeePowerball.txt", "a")
     employeePowerball.write(bytes(line))
+    employeePowerball.write("\n")
     employeePowerball.close()
+
+def findNumFrequency():
+    employeePowerball = open("employeePowerball.txt", "r")
+    text_in_file = employeePowerball.read()
+    print ("hi" + str(Counter(text_in_file).most_common(5)))
 
 def printFromFile():
     employeePowerball = open("employeePowerball.txt", "r")
     text_in_file = employeePowerball.read()
     print(text_in_file)
 
-# getNameAndNumbersFromUser()
+getNameAndNumbersFromUser()
 # writeToFile(nameAndFavNums)
+# findNumFrequency()
 # printFromFile()
